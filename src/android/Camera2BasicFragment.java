63474a79df3a89c87bcecacd9a8338ec09b30fe3
Andre Abascal <andreabascal@gmail.com>
2018-11-28 15:36:26 -0200

    ...

--- src/android/Camera2BasicFragment.java
+++ src/android/Camera2BasicFragment.java
@@ -543,7 +543,7 @@ public class Camera2BasicFragment extends Fragment implements View.OnClickListen
                     ImageView imgView = new ImageView(ctx);
                     imgView.setImageBitmap(myBitmap);
                     imgView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-					HorizontalScrollView hsv = (HorizontalScrollView) activity.findViewById(activity.getResources().getIdentifier("hsv", "id", activity.getPackageName()));
+					// ScrollView hsv = (ScrollView) activity.findViewById(activity.getResources().getIdentifier("hsv", "id", activity.getPackageName()));
 					LinearLayout.LayoutParams layout = new LinearLayout.LayoutParams(squareDim,squareDim);
                     layout.setMargins(dpToPx(16,ctx),dpToPx(16,ctx),dpToPx(16,ctx),dpToPx(16,ctx));
                     imgView.setLayoutParams(layout);
