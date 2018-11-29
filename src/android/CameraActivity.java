7a8553aaf3a42498e98a67ddc8498d97d0ea9f88
Andre Abascal <andreabascal@gmail.com>
2018-11-29 18:35:57 -0200

    ...

--- src/android/CameraActivity.java
+++ src/android/CameraActivity.java
@@ -16,7 +16,7 @@
 
 package cordova.plugin.multicamera;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Build;
