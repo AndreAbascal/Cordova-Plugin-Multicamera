8bab991d4cfa0f3f1fb4e7a5c0372ca0cf056fa0
Andre Abascal <andreabascal@gmail.com>
2019-01-14 15:43:30 -0200

    ...

--- www/MultiCamera.js
+++ www/MultiCamera.js
@@ -4,5 +4,5 @@ var service = "MultiCamera";
 
 exports.open = function(success,error,params){
 	console.log("MultiCamera.js... openCamera params: ",params);
-	exec(success, error, service, 'open', []);
+	exec(success, error, service, 'open', [params]);
 };
