221bdd5cd74cf429f2655bafe06566d60ba11714
Andre Abascal <andreabascal@gmail.com>
2019-01-15 08:58:27 -0200

    ...

--- src/ios/MultiCamera.swift
+++ src/ios/MultiCamera.swift
@@ -1,6 +1,6 @@
 import Foundation
 @objc(MultiCamera) class MultiCamera: CDVPlugin {
-    func startcamera(_ command: CDVInvokedUrlCommand) {
+    func open(_ command: CDVInvokedUrlCommand) {
         let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "message");
         let cameraVC = CameraViewController();
         self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
